715ac6adba5b9e3a6c7f9133bda30ee477d23d55
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2024-11-22 05:44:36 -0600

    implement schema errMsg validator

--- backend/src/server/plugins/schema-validator.ts
+++ backend/src/server/plugins/schema-validator.ts
@@ -6,18 +6,65 @@ import {HTTP_ERRORS} from '@shared/errors/http-errors.js';
 import {SetErrorFunction} from '@sinclair/typebox/errors';
 import {APP_ERRORS} from '@shared/errors/app-errors.js';
 
+/**
+ * Checks if a given typebox schema contains errMsg recursively
+ * @throws {APP_ERRORS.UNDEFINED_SCHEMA_ERROR_MESSAGE}
+ * @param schema
+ */
+export function validateSchemaFormat(schema: TSchema) {
+  if (typeof schema.errMsg !== 'string') {
+    throw new APP_ERRORS.UNDEFINED_SCHEMA_ERROR_MESSAGE().causedBy(schema);
+  }
+
+  if (schema.type === 'object') {
+    if (schema.properties) {
+      for (const key of Object.keys(schema.properties)) {
+        validateSchemaFormat(schema.properties[key]);
+      }
+    }
+
+    if (schema.patternProperties) {
+      for (const key of Object.keys(schema.patternProperties)) {
+        validateSchemaFormat(schema.patternProperties[key]);
+      }
+    }
+  }
+
+  if (schema.type === 'array') {
+    if (Array.isArray(schema.items)) {
+      for (const s of schema.items) {
+        validateSchemaFormat(s);
+      }
+    } else {
+      validateSchemaFormat(schema.items);
+    }
+  }
+
+  if (Array.isArray(schema.allOf)) {
+    for (const s of schema.allOf) {
+      validateSchemaFormat(s);
+    }
+  }
+
+  if (Array.isArray(schema.anyOf)) {
+    for (const s of schema.anyOf) {
+      validateSchemaFormat(s);
+    }
+  }
+
+  if (schema.not) {
+    validateSchemaFormat(schema.not);
+  }
+}
+
 // Custom schema validator to allow for custom error handling
 export default fastifyPlugin((fastify: AppFastifyInstance) => {
-  SetErrorFunction(e => {
-    if (e.schema.errMsg && typeof e.schema.errMsg === 'string') {
-      return e.schema.errMsg;
-    }
-    throw new APP_ERRORS.UNDEFINED_SCHEMA_ERROR_MESSAGE().causedBy(e);
-  });
+  SetErrorFunction(e => e.schema.errMsg);
 
   fastify.setValidatorCompiler(schemaDef => {
+    validateSchemaFormat(schemaDef.schema as TSchema);
     const validator = TypeBoxValidatorCompiler(schemaDef as FastifyRouteSchemaDef<TSchema>);
-    // console.log(schemaDef.schema.properties.record)
+
     return (...args) => {
       const result = validator(...args) as unknown as {
         value: unknown;
