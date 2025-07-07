0d37b3684bf617753c7528d110d268d1ca4e65a0
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-07-06 19:11:22 -0700

    fix early subtitle issue

--- frontend/public/convert_to_hls.sh
+++ frontend/public/convert_to_hls.sh
@@ -9,12 +9,15 @@ set -e
 ffmpeg -i $1 -map 0:v:0 -map 0:a:0 -map 0:s:0 -c copy single_track.mkv
 
 # Assuming input video is h264 and audio is aac
+# -c:s webvtt because hls requires webvtt subtitles
+# -muxdelay 0 to ensure ffmpeg doesn't add subtitle offset
 ffmpeg -i single_track.mkv \
     -map 0:v:0 -map 0:a:0 -map 0:s:0 \
     -c:v copy -c:a copy -c:s webvtt \
     -var_stream_map "v:0,a:0,s:0" \
     -hls_playlist_type vod \
     -hls_time 6 \
+    -muxdelay 0 \
     -master_pl_name master.m3u8 \
     -f hls $2/stream_%v.m3u8
 
@@ -34,5 +37,6 @@ stream_0.m3u8
 #    -var_stream_map "v:0,a:0,s:0" \
 #    -hls_playlist_type vod \
 #    -hls_time 6 \
+#    -muxdelay 0 \
 #    -master_pl_name master.m3u8 \
 #    -f hls $2/video_%v.m3u8
